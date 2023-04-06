290f7f68939fb70188285a7c4128a0285e385cf6
gcostacoelho <gcostacoelho2003@gmail.com>
2023-04-06 09:39:04 -0300

    insert-tailwind-in-project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { useState } from 'react'
+import './styles/main.css';
 
 function App() {
   return (
     <div className="App">
-      <h3>Hello World</h3>
+      <h3 className='text-3xl font-bold'>Hello World</h3>
     </div>
   )
 }
