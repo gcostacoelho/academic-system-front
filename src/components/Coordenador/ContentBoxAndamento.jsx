f82dd9a27a824cee7666b7b82b9f4f986c854945
gcostacoelho <gcostacoelho2003@gmail.com>
2023-04-12 21:28:43 -0300

    update-ContentBox

--- src/components/Coordenador/ContentBoxAndamento.jsx
+++ src/components/Coordenador/ContentBoxAndamento.jsx
@@ -1,4 +1,27 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export function ContentBoxAndamento(props) {
+    const [alunosInsc, setAlunosInsc] = useState([])
+
+    { /*useEffect(() => {
+        axios('', {
+            params: {
+                disciplina: props.disciplina
+            }
+        }).then(resp => {
+            setAlunosInsc(resp.data)
+        });
+    }, []);
+    */}
+
+    useEffect(() => {
+        axios('https://api.breakingbadquotes.xyz/v1/quotes/0').then(resp => {
+            setAlunosInsc(resp.data)
+        });
+    }, [])
+
+
     return (
         <div>
             <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
@@ -18,19 +41,24 @@ export function ContentBoxAndamento(props) {
                 </thead>
 
                 <tbody>
-                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                        <td scope="row" className="px-6 py-4">
-                            {props.dataInsc}
-                        </td>
-                        <td scope="row" className="px-6 py-4">
-                            {props.nomeAluno}
-                        </td>
-                        <td scope="row" className="px-6 py-4">
-                            {props.matricula}
-                        </td>
-                    </tr>
+                    {
+                        alunosInsc > 0 && alunosInsc.map((aluno) => {
+                            return (
+                                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                    <td scope="row" className="px-6 py-4">
+                                        {aluno.dataInsc}
+                                    </td>
+                                    <td scope="row" className="px-6 py-4">
+                                        {aluno.nomeAluno}
+                                    </td>
+                                    <td scope="row" className="px-6 py-4">
+                                        {aluno.matricula}
+                                    </td>
+                                </tr>
+                            );
+                        })
+                    }
                 </tbody>
-
             </table>
         </div>
     );
