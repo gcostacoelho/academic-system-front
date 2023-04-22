3d8e910c38f38518c9709f6cc27995164aebb62b
gcostacoelho <gcostacoelho2003@gmail.com>
2023-04-22 16:43:36 -0300

    finally-component-ContentBox

--- src/components/Coordenador/ListaEspera/ContentBoxListaEspera.jsx
+++ src/components/Coordenador/ListaEspera/ContentBoxListaEspera.jsx
@@ -2,7 +2,8 @@ import * as ToggleGroup from '@radix-ui/react-toggle-group';
 import { useState } from "react";
 
 export function ContentBoxListaEspera(props) {
-    const [weekDays, setWeekDays] = useState([])
+    const [weekDays, setWeekDays] = useState([]);
+    const [data, setData] = useState([]);
 
     async function handlePostApi(event) {
         event.preventDefault();
@@ -27,11 +28,11 @@ export function ContentBoxListaEspera(props) {
 
             <br />
 
-            <div className="flex gap-6">
+            <div>
                 <div className="flex flex-col gap-2">
                     <label>Qual o dia da turma?</label>
 
-                    <ToggleGroup.Root type="multiple" value={weekDays} className="grid grid-cols-5 gap-2" onValueChange={setWeekDays}>
+                    <ToggleGroup.Root type="multiple" value={weekDays} className="flex flex-row gap-2" onValueChange={setWeekDays}>
                         <ToggleGroup.Item
                             disabled={weekDays.length == 0 || weekDays.includes('1') ? false : true}
                             value="1"
@@ -74,14 +75,68 @@ export function ContentBoxListaEspera(props) {
                         </ToggleGroup.Item>
                     </ToggleGroup.Root>
                 </div>
-
+                <br />
                 <div className="flex flex-col gap-2 flex-1">
                     <label htmlFor="hourPlaying">Qual horário do dia?</label>
                     <div className="grid grid-cols-2 gap-2">
-                        <input type="time" min="07:00" max="22:35" placeholder="De" id="HourSart" name="hoursStart" />
-                        <input type="time" min="07:00" max="22:35" placeholder="Até" id="hourEnd" name="hourEnd" />
+                        <input className='p-1 rounded' type="time" min="07:00" max="22:35" placeholder="De" id="HourSart" name="hoursStart" />
+                        <input className='p-1 rounded' type="time" min="07:00" max="22:35" placeholder="Até" id="hourEnd" name="hourEnd" />
                     </div>
                 </div>
+                <br />
+                <div className="flex flex-col gap-2">
+                    <label>Escolha o professor: </label>
+                    <select
+                        id="prof"
+                        name="prof"
+                        placeholder="Escolha o professor"
+                        className="p-2 rounded text-sm placeholder:text-zinc-500"
+                    >
+                        <option defaultValue="" value="">Escolha o professor</option>
+                        <option defaultValue="" value="">Vendramel</option>
+                        <option defaultValue="" value="">Irineu</option>
+
+                        {/*
+                            data.map(game => {
+                                return <option key={data.id} value={data.id}>{data.title}</option>
+                            })
+                        */}
+                    </select>
+                </div>
+
+                <br />
+
+                <div className="flex flex-col gap-2">
+                    <label>Escolha a Sala: </label>
+                    <select
+                        id="prof"
+                        name="prof"
+                        placeholder="Escolha a Sala"
+                        className="p-2 rounded text-sm placeholder:text-zinc-500"
+                    >
+                        <option defaultValue="" value="">Escolha a Sala</option>
+                        <option defaultValue="" value="">A405</option>
+                        <option defaultValue="" value="">B405</option>
+
+                        {/*
+                            data.map(game => {
+                                return <option key={data.id} value={data.id}>{data.title}</option>
+                            })
+                        */}
+                    </select>
+                </div>
+
+                <br />
+
+                <div>
+                    <label htmlFor="">Quantidade de alunos para essa turma: </label>
+                    <input className='p-1 rounded w-1/6' placeholder='Qtd. Aluno' type="number" min="1" max={props.qtdAlunosLista} />
+                </div>
+
+                <div className="float-right">
+                    <button type="submit" className="bg-blue-500 p-1 rounded">Cadastrar nova turma</button>
+                </div>
+
             </div>
         </form >
     );
